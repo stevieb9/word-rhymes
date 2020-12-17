098d2019bfdd772b5f71d42687fc604f173e9ead
Steve Bertrand <steveb@cpan.org>
2020-12-17 08:05:39 -0800

    Backend additions
    - Travis-CI
    - Git information
    - Prerequisites

--- lib/Word/Rhymes.pm
+++ lib/Word/Rhymes.pm
@@ -1,21 +1,54 @@
 package Word::Rhymes;
 
-use 5.006;
 use strict;
 use warnings;
+
+our $VERSION = '0.01';
+
+use JSON;
+use HTTP::Request;
+use LWP::UserAgent;
+
+use constant DEBUG => 0;
+
+my $max_results = 50;
+my $min_score = 300;
+
+die "Need param!\n" if ! @ARGV;
+my $word = $ARGV[0];
+
+my $uri = "https://rhymebrain.com/talk?function=getRhymes&word=$word&maxResults=$max_results";
+
+my $ua = LWP::UserAgent->new;
+my $req = HTTP::Request->new( 'GET', $uri );
+
+my $response = $ua->request($req);
+
+if ($response->is_success) {
+    my $orig = $response->decoded_content;
+    my $data = decode_json $response->decoded_content;
+
+    for (@$data) {
+        say "$_->{word}: $_->{score}" if $_->{score} >= $min_score && DEBUG;
+        say "$_->{word}" if $_->{score} >= $min_score;
+    }
+}
+else {
+    print "Invalid response\n\n";
+}
+
+sub __placeholder {}
+
+1;
+__END__
 
 =head1 NAME
 
 Word::Rhymes - The great new Word::Rhymes!
 
-=head1 VERSION
-
-Version 0.01
-
-=cut
-
-our $VERSION = '0.01';
-
+=for html
+<a href="http://travis-ci.com/stevieb9/words-rhyme"><img src="https://www.travis-ci.com/stevieb9/words-rhyme.svg?branch=master"/>
+<a href='https://coveralls.io/github/stevieb9/words-rhyme?branch=master'><img src='https://coveralls.io/repos/stevieb9/words-rhyme/badge.svg?branch=master&service=github' alt='Coverage Status' /></a>
 
 =head1 SYNOPSIS
 
@@ -33,67 +66,11 @@ Perhaps a little code snippet.
 A list of functions that can be exported.  You can delete this section
 if you don't export anything, such as for a purely object-oriented module.
 
-=head1 SUBROUTINES/METHODS
-
-=head2 function1
-
-=cut
-
-sub function1 {
-}
-
-=head2 function2
-
-=cut
-
-sub function2 {
-}
+=head1 METHODS/FUNCTIONS
 
 =head1 AUTHOR
 
 Steve Bertrand, C<< <steveb at cpan.org> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-word-rhymes at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Word-Rhymes>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Word::Rhymes
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker (report bugs here)
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Word-Rhymes>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Word-Rhymes>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Word-Rhymes>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Word-Rhymes/>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
 
 =head1 LICENSE AND COPYRIGHT
 
@@ -104,38 +81,3 @@ under the terms of the the Artistic License (2.0). You may obtain a
 copy of the full license at:
 
 L<http://www.perlfoundation.org/artistic_license_2_0>
-
-Any use, modification, and distribution of the Standard or Modified
-Versions is governed by this Artistic License. By using, modifying or
-distributing the Package, you accept this license. Do not use, modify,
-or distribute the Package, if you do not accept this license.
-
-If your Modified Version has been derived from a Modified Version made
-by someone other than you, you are nevertheless required to ensure that
-your Modified Version complies with the requirements of this license.
-
-This license does not grant you the right to use any trademark, service
-mark, tradename, or logo of the Copyright Holder.
-
-This license includes the non-exclusive, worldwide, free-of-charge
-patent license to make, have made, use, offer to sell, sell, import and
-otherwise transfer the Package with respect to any patent claims
-licensable by the Copyright Holder that are necessarily infringed by the
-Package. If you institute patent litigation (including a cross-claim or
-counterclaim) against any party alleging that the Package constitutes
-direct or contributory patent infringement, then this Artistic License
-to you shall terminate on the date that such litigation is filed.
-
-Disclaimer of Warranty: THE PACKAGE IS PROVIDED BY THE COPYRIGHT HOLDER
-AND CONTRIBUTORS "AS IS' AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES.
-THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
-PURPOSE, OR NON-INFRINGEMENT ARE DISCLAIMED TO THE EXTENT PERMITTED BY
-YOUR LOCAL LAW. UNLESS REQUIRED BY LAW, NO COPYRIGHT HOLDER OR
-CONTRIBUTOR WILL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, OR
-CONSEQUENTIAL DAMAGES ARISING IN ANY WAY OUT OF THE USE OF THE PACKAGE,
-EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-
-=cut
-
-1; # End of Word::Rhymes
